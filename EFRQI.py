8139f4be281b4f9b98ea88c1ea93e193ccba48cc
rishab ramanathan <rishabramanathan@mahindrauniversity.edu.in>
2023-08-07 12:19:01 +0530

    semantic typos / EFRQI (not sure about correctness)

--- EFRQI.py
+++ EFRQI.py
@@ -9,7 +9,6 @@ import math
 from image_parser import *
 from binary_helper import *
 
-
 #image initialization
 img_filepath = "assets/test.jpg"
 dim = 16
@@ -20,14 +19,15 @@ image = get_image_pixel_array(img_filepath,dim,binflag=True)
 #simulation backend
 backendQasm = Aer.get_backend('qasm_simulator')
 
-#circuit
+#quantumcircuit
 pos_bits = int(math.log(dim,2))
 pixelMap = QuantumRegister(8,'pixelMap')
 position = QuantumRegister(2*pos_bits,'position')
+auxillary = QuantumRegister(1,'aux')
 classic_bits = 2*pos_bits + 8
 measure = ClassicalRegister(classic_bits,'measure')
-qc = QuantumCircuit(pixelMap, position, measure)
-qc.h(position)
+qc = QuantumCircuit(pixelMap, auxillary, position, measure)
+
 cnx_gate = XGate().control(2*pos_bits)
 
 pixel_id = 0
@@ -35,20 +35,25 @@ for pixel in image:
     qc.barrier()
     pixel_id_bin = decimal_to_binary(pixel_id,2*pos_bits)
 
+    control_parameter = [*range(9, 9 + 2*pos_bits,1)]
+    control_parameter.append(8)
+
     pos_id = 0
     while(pos_id < 2*pos_bits):
         if(pixel_id_bin[pos_id] == '1'):
             qc.x(position[pos_id])
         pos_id += 1
+    
+    qc.append(cnx_gate, control_parameter)
 
     bin_id = 0
     for binary in pixel:
         if(binary == '1'):
-            control_parameter = [*range(8, 8 + int(2*pos_bits), 1)]
-            control_parameter.append(bin_id)
-            qc.append(cnx_gate, control_parameter)
+            qc.cnot(8, bin_id)
         bin_id += 1
-    
+
+    qc.append(cnx_gate, control_parameter)
+
     pos_id = 0
     while(pos_id < 2*pos_bits):
         if(pixel_id_bin[pos_id] == '1'):
@@ -59,16 +64,12 @@ for pixel in image:
 
 qc.barrier()
 
-qc.measure(range(16),range(16))
+qc.measure(range(8),range(8))
 
 #simulate
 aer_sim = Aer.get_backend('aer_simulator')
 job = execute(qc,aer_sim,shots=16384)
 result_neqr = job.result()
 counts_neqr = result_neqr.get_counts()
-#print(qc)
-#print(counts_neqr)
-
-#translate counts back to image
-img_translate = parse_to_image_array(f'{counts_neqr}',dim,pos_bits) #pass counts data as string
-#display_image(img_translate,dim)
+print(qc)
+print(counts_neqr)
